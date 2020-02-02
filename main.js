ed37b7c09654ec09e22be7f4591cfdb263ca6f1c
GemnaGame <gemnagame@gmail.com>
2020-02-02 23:18:00 +0900

    axios, cheerio 이용한 크롤링 테스트

--- main.js
+++ main.js
@@ -6,6 +6,43 @@ const path = require('path')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+const axios = require("axios");
+const cheerio = require("cheerio");
+const log = console.log;
+
+const getHtml = async () => {
+  try {
+    //return await axios.get("https://www.yna.co.kr/sports/all");
+    return await axios.get("http://www.okcashbag.com/life/exchange/exchangeMain.do");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getHtml()
+  .then(html => {
+    let ulList = [];
+    const $ = cheerio.load(html.data);
+    //const $bodyList = $("div.headline-list ul").children("li.section02");
+    const $bodyList = $("div.point-list-wrap2 ul").children("li");
+
+    $bodyList.each(function(i, elem) {
+      ulList[i] = {
+        title: $(this).find('dt').text()
+          // title: $(this).find('strong.news-tl a').text(),
+          // url: $(this).find('strong.news-tl a').attr('href'),
+          // image_url: $(this).find('p.poto a img').attr('src'),
+          // image_alt: $(this).find('p.poto a img').attr('alt'),
+          // summary: $(this).find('p.lead').text().slice(0, -11),
+          // date: $(this).find('span.p-time').text()
+      };
+    });
+
+    const data = ulList.filter(n => n.title);
+    return data;
+  })
+  .then(res => log(res));
+
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
