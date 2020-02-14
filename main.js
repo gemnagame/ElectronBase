e9bee59218f84e12d5d86355f327a0c00a535c71
GemnaGame <gemnagame@gmail.com>
2020-02-14 18:17:54 +0900

    webview 이용

--- main.js
+++ main.js
@@ -24,7 +24,8 @@ function createWindow () {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      webviewTag : true
     }
   })
 
