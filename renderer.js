ab8d4e326bbc26a10b83b9e9797707419823c672
GemnaGame <gemnagame@gmail.com>
2020-02-22 21:17:52 +0900

    html5 적용, 검색엔진 최적화, UI 이벤트 함수 추가, enum 추가(BANK_TYPE, PAGE_TYPE)

--- renderer.js
+++ renderer.js
@@ -7,6 +7,37 @@
 
 const {ipcRenderer} = require('electron')
 
+var BANK_TYPE = {
+    KAKAO_BANK : 1,
+    WOORI_BANK : 2,
+    SHINHAN_BANK : 3,
+    NONGHYUP_BANK : 4
+}
+
+var PAGE_TYPE = {
+    EVENT_PAGE : 1,
+    PRODUCT_PAGE : 2
+};
+
+var g_bank_type = BANK_TYPE.KAKAO_BANK;
+var g_page_type = PAGE_TYPE.EVENT_PAGE;//전역변수인데 클래스로 변경?? 구조 고민 ㄱㄱ
+
+function OnChangeBankType() {
+    var element = document.querySelector('#bank_type');
+    var bankType = element.value.toUpperCase();
+  
+    ChangeBankType(BANK_TYPE[bankType]);
+}
+
+function ChangeBankType(bank_type) {
+    g_bank_type = bank_type;
+    alert(g_bank_type);
+}
+
+function OnClickPageType(page_type) {
+    g_page_type = page_type;
+    alert(g_page_type);
+}
 
 function createDivElementFromHTML(htmlString) {
     var div = document.createElement('div');
